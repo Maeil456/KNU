304c787bc5cafc0fa807b346a6d01d21b000303c
Maeil456 <ghdrbs000@gmail.com>
2023-08-02 23:04:37 +0900

    0802

--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -7,59 +7,69 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Button
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_OVERLAY_PERMISSION = 101
+    private lateinit var overlayPermissionLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnEpisode1 = findViewById<Button>(R.id.btnkakao)
-        val btnEpisode2 = findViewById<Button>(R.id.btnnaver)
-        // Add more buttons for each episode as needed
-
-        btnEpisode1.setOnClickListener {
-            val kakaoTalkIntent = packageManager.getLaunchIntentForPackage("com.kakao.talk")
-            if (kakaoTalkIntent != null) {
-                startActivity(kakaoTalkIntent)
-                val floatingImage = FloatingImage(this)
-                floatingImage.show(100, 200) // Set the desired X and Y position for the floating image
-            } else {
-                // Handle the case when KakaoTalk is not installed
-                // You can show an error message or direct the user to install KakaoTalk
-                val link = "https://play.google.com/store/apps/details?id=com.kakao.talk&hl=ko&gl=US"
-                val kakaoTalkIntent = Intent(Intent.ACTION_VIEW).apply {
-                    data = Uri.parse(link)
+        overlayPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            // Check if the permission was granted by the user
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (Settings.canDrawOverlays(this)) {
+                    // Permission granted, show the floating image
+                    //showFloatingImage()
+                } else {
+                    // Permission denied, handle it gracefully
+                    // For example, show an alternative UI or a message to the user
                 }
-                startActivity(kakaoTalkIntent)
+            }
+
+            val btnEpisode1 = findViewById<Button>(R.id.btnkakao)
+            val btnEpisode2 = findViewById<Button>(R.id.btnnaver)
+            // Add more buttons for each episode as needed
+
+            btnEpisode1.setOnClickListener {
+                val packageName = "com.kakao.talk"
+                startEpisodeIntent(packageName)
+                val floatingImage = FloatingImage(this)
+                floatingImage.show(100, 200)
+            }
+            //haha
+            btnEpisode2.setOnClickListener {
+                // Similar logic as btnEpisode1
+                // Launch KakaoTalk with the appropriate episode intent and display the floating image
+                val packageName = "com.nhn.android.search"
+                startEpisodeIntent(packageName)
                 val floatingImage = FloatingImage(this)
                 floatingImage.show(100, 200)
             }
         }
-    //haha
-        btnEpisode2.setOnClickListener {
-            // Similar logic as btnEpisode1
-            // Launch KakaoTalk with the appropriate episode intent and display the floating image
-        }
 
-        // Implement click listeners for other buttons as needed for each episode
-    }
-    private fun checkOverlayPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (!Settings.canDrawOverlays(this)) {
-                // You need to request the permission at runtime
-                val intent = Intent(
-                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:" + packageName)
-                )
-                startActivityForResult(intent, REQUEST_OVERLAY_PERMISSION)
+
+        fun startEpisodeIntent(packageName: String) {
+            val intent = packageManager.getLaunchIntentForPackage(packageName)
+            if (intent != null) {
+                startActivity(intent)
             } else {
-                // Permission already granted, show the floating image
-                showFloatingImage()
+                // Handle the case when the target app is not installed
+                // You can show an error message or direct the user to install the app
+                val link = "https://play.google.com/store/apps/details?id=$packageName"
+                val intent = Intent(Intent.ACTION_VIEW).apply {
+                    data = Uri.parse(link)
+                }
+                startActivity(intent)
             }
-        } else {
-            // On pre-Marshmallow devices, no special permission is required
-            showFloatingImage()
         }
     }
 }
