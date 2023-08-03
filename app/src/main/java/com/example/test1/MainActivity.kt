29b4ac31b25507253510a87adee7e8d60856631f
Maeil456 <ghdrbs000@gmail.com>
2023-08-03 20:30:41 +0900

    0803

--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.test1
 
-// MainActivity.kt
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
@@ -10,6 +9,7 @@ import android.widget.Button
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -32,44 +32,76 @@ class MainActivity : AppCompatActivity() {
                 } else {
                     // Permission denied, handle it gracefully
                     // For example, show an alternative UI or a message to the user
+                    showOverlayPermissionAlert()
                 }
-            }
-
-            val btnEpisode1 = findViewById<Button>(R.id.btnkakao)
-            val btnEpisode2 = findViewById<Button>(R.id.btnnaver)
-            // Add more buttons for each episode as needed
-
-            btnEpisode1.setOnClickListener {
-                val packageName = "com.kakao.talk"
-                startEpisodeIntent(packageName)
-                val floatingImage = FloatingImage(this)
-                floatingImage.show(100, 200)
-            }
-            //haha
-            btnEpisode2.setOnClickListener {
-                // Similar logic as btnEpisode1
-                // Launch KakaoTalk with the appropriate episode intent and display the floating image
-                val packageName = "com.nhn.android.search"
-                startEpisodeIntent(packageName)
-                val floatingImage = FloatingImage(this)
-                floatingImage.show(100, 200)
             }
         }
 
+        val btnEpisode1 = findViewById<Button>(R.id.btnkakao)
+        val btnEpisode2 = findViewById<Button>(R.id.btnnaver)
+        // Add more buttons for each episode as needed
 
-        fun startEpisodeIntent(packageName: String) {
-            val intent = packageManager.getLaunchIntentForPackage(packageName)
-            if (intent != null) {
-                startActivity(intent)
-            } else {
-                // Handle the case when the target app is not installed
-                // You can show an error message or direct the user to install the app
-                val link = "https://play.google.com/store/apps/details?id=$packageName"
-                val intent = Intent(Intent.ACTION_VIEW).apply {
-                    data = Uri.parse(link)
-                }
-                startActivity(intent)
+        btnEpisode1.setOnClickListener {
+            val packageName = "com.kakao.talk"
+            startEpisodeIntent(packageName)
+            val floatingImage = FloatingImage(this)
+            floatingImage.show(100, 200)
+        }
+
+        btnEpisode2.setOnClickListener {
+            // Similar logic as btnEpisode1
+            // Launch KakaoTalk with the appropriate episode intent and display the floating image
+            val packageName = "com.nhn.android.search"
+            startEpisodeIntent(packageName)
+            val floatingImage = FloatingImage(this)
+            floatingImage.show(100, 200)
+        }
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun checkOverlayPermission() {
+        if (!Settings.canDrawOverlays(this)) {
+            // You need to request the permission at runtime
+            showOverlayPermissionAlert()
+        } else {
+            // Permission already granted, show the floating image
+            //showFloatingImage()
+        }
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun showOverlayPermissionAlert() {
+        val alertBuilder = AlertDialog.Builder(this)
+        alertBuilder.setTitle("Permission Required")
+        alertBuilder.setMessage("This app requires the overlay permission to function properly.")
+        alertBuilder.setPositiveButton("Grant Permission") { dialog, which ->
+            requestOverlayPermission()
+        }
+        alertBuilder.setNegativeButton("Cancel") { dialog, which ->
+            // Handle the case when the user cancels the permission request
+            // For example, show an alternative UI or a message to the user
+        }
+        val alertDialog = alertBuilder.create()
+        alertDialog.show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun requestOverlayPermission() {
+        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
+        overlayPermissionLauncher.launch(intent)
+    }
+
+    fun startEpisodeIntent(packageName: String) {
+        val intent = packageManager.getLaunchIntentForPackage(packageName)
+        if (intent != null) {
+            startActivity(intent)
+        } else {
+            // Handle the case when the target app is not installed
+            // You can show an error message or direct the user to install the app
+            val link = "https://play.google.com/store/apps/details?id=$packageName"
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(link)
             }
+            startActivity(intent)
         }
     }
 }
+
