66b61a2f40b0232eba3292e1cd70f390b5bae16b
이종윤 <whddbs3456@gmail.com>
2023-08-05 17:42:42 +0900

    0805

--- app/src/main/java/com/example/test1/FloatingImageService.kt
+++ app/src/main/java/com/example/test1/FloatingImageService.kt
@@ -6,6 +6,7 @@ import android.graphics.PixelFormat
 import android.os.Build
 import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager
 import android.view.accessibility.AccessibilityEvent
@@ -15,17 +16,19 @@ class FloatingImageService : AccessibilityService() {
 
     private lateinit var windowManager: WindowManager
     private lateinit var floatingView: View
+    private lateinit var imageView: ImageView
     private var isShowing = false
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
         // Check if the desired app is launched
-        if (event?.packageName == "com.kakao.talk") {
+        if (event?.packageName == "com.kakao.talk" || event?.packageName == "com.nhn.android.search") {
             // Show the floating image when the desired app is launched
-            showFloatingImage(100,200)
+            showFloatingImage(100, 200)
+        } else {
+            // Hide the floating image when the desired app is closed or goes to the background
+            hideFloatingImage()
         }
     }
-
-
 
     override fun onInterrupt() {
         // Handle service interruption
@@ -59,6 +62,40 @@ class FloatingImageService : AccessibilityService() {
         floatingView = LayoutInflater.from(this).inflate(R.layout.floating_image_layout, null)
         windowManager.addView(floatingView, params)
         isShowing = true
+
+        // Add touch listener to handle dragging and dismissing the floating image
+        imageView.setOnTouchListener(object : View.OnTouchListener {
+            private var initialX: Int = 0
+            private var initialY: Int = 0
+            private var initialTouchX: Float = 0f
+            private var initialTouchY: Float = 0f
+
+            override fun onTouch(v: View, event: MotionEvent): Boolean {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        initialX = params.x
+                        initialY = params.y
+                        initialTouchX = event.rawX
+                        initialTouchY = event.rawY
+                        return true
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        params.x = initialX + (event.rawX - initialTouchX).toInt()
+                        params.y = initialY + (event.rawY - initialTouchY).toInt()
+                        windowManager.updateViewLayout(floatingView, params)
+                        return true
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        // Implement dismissing logic here if needed
+                        return true
+                    }
+                }
+                return false
+            }
+        })
+
+        windowManager.addView(floatingView, params)
+        isShowing = true
     }
 
     private fun hideFloatingImage() {
@@ -67,5 +104,5 @@ class FloatingImageService : AccessibilityService() {
         isShowing = false
     }
 
-    // Add other methods as needed, e.g., to update the position of the floating image based on touch events
+    // Add other methods as needed
 }
--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -1,65 +1,109 @@
 package com.example.test1
 
-// MainActivity.kt
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Button
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_OVERLAY_PERMISSION = 101
+    private lateinit var overlayPermissionLauncher: ActivityResultLauncher<Intent>
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        overlayPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            // Check if the permission was granted by the user
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (Settings.canDrawOverlays(this)) {
+                    // Permission granted, show the floating image
+                    //showFloatingImage()
+                } else {
+                    // Permission denied, handle it gracefully
+                    // For example, show an alternative UI or a message to the user
+                    showOverlayPermissionAlert()
+                }
+            }
+        }
 
         val btnEpisode1 = findViewById<Button>(R.id.btnkakao)
         val btnEpisode2 = findViewById<Button>(R.id.btnnaver)
         // Add more buttons for each episode as needed
 
         btnEpisode1.setOnClickListener {
-            val kakaoTalkIntent = packageManager.getLaunchIntentForPackage("com.kakao.talk")
-            if (kakaoTalkIntent != null) {
-                startActivity(kakaoTalkIntent)
-                val floatingImage = FloatingImage(this)
-                floatingImage.show(100, 200) // Set the desired X and Y position for the floating image
-            } else {
-                // Handle the case when KakaoTalk is not installed
-                // You can show an error message or direct the user to install KakaoTalk
-                val link = "https://play.google.com/store/apps/details?id=com.kakao.talk&hl=ko&gl=US"
-                val kakaoTalkIntent = Intent(Intent.ACTION_VIEW).apply {
-                    data = Uri.parse(link)
-                }
-                startActivity(kakaoTalkIntent)
-                val floatingImage = FloatingImage(this)
-                floatingImage.show(100, 200)
-            }
+            val packageName = "com.kakao.talk"
+            startEpisodeIntent(packageName)
+            checkOverlayPermission()
+            val floatingImageService = FloatingImageService()
+            floatingImageService.showFloatingImage(100, 200)
         }
-    //haha wow
+
         btnEpisode2.setOnClickListener {
             // Similar logic as btnEpisode1
             // Launch KakaoTalk with the appropriate episode intent and display the floating image
+            val packageName = "com.nhn.android.search"
+            startEpisodeIntent(packageName)
+            val floatingImageService = FloatingImageService()
+            floatingImageService.showFloatingImage(100, 200)
         }
-
-        // Implement click listeners for other buttons as needed for each episode
     }
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun checkOverlayPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (!Settings.canDrawOverlays(this)) {
-                // You need to request the permission at runtime
-                val intent = Intent(
-                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:" + packageName)
-                )
-                startActivityForResult(intent, REQUEST_OVERLAY_PERMISSION)
-            } else {
-                // Permission already granted, show the floating image
-                showFloatingImage()
-            }
+        if (!Settings.canDrawOverlays(this)) {
+            // You need to request the permission at runtime
+            showOverlayPermissionAlert()
         } else {
-            // On pre-Marshmallow devices, no special permission is required
-            showFloatingImage()
+            // Permission already granted, show the floating image
+            //showFloatingImage()
+        }
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun showOverlayPermissionAlert() {
+        val alertBuilder = AlertDialog.Builder(this)
+        alertBuilder.setTitle("Permission Required")
+        alertBuilder.setMessage("This app requires the overlay permission to function properly.")
+        alertBuilder.setPositiveButton("Grant Permission") { dialog, which ->
+            requestOverlayPermission()
+        }
+        alertBuilder.setNegativeButton("Cancel") { dialog, which ->
+            // Handle the case when the user cancels the permission request
+            // For example, show an alternative UI or a message to the user
+        }
+        val alertDialog = alertBuilder.create()
+        alertDialog.show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun requestOverlayPermission() {
+        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
+        overlayPermissionLauncher.launch(intent)
+    }
+
+    fun startEpisodeIntent(packageName: String) {
+        val intent = packageManager.getLaunchIntentForPackage(packageName)
+        if (intent != null) {
+            startActivity(intent)
+        } else {
+            // Handle the case when the target app is not installed
+            // You can show an error message or direct the user to install the app
+            val link = "https://play.google.com/store/apps/details?id=$packageName"
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(link)
+            }
+            startActivity(intent)
         }
     }
 }
+
