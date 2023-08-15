37710e7a3bbcb8a340b739ee673a1b4f68c12b5f
Maeil456 <ghdrbs000@gmail.com>
2023-08-15 12:32:05 +0900

    0815

--- app/src/main/java/com/example/test1/FloatingImageService.kt
+++ app/src/main/java/com/example/test1/FloatingImageService.kt
@@ -5,33 +5,40 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.PixelFormat
 import android.os.Build
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.WindowManager
-import android.view.accessibility.AccessibilityEvent
+import android.view.*
 import android.widget.ImageView
+import android.view.accessibility.AccessibilityEvent
+import android.widget.Toast
+
+import kotlin.collections.ArrayList
 
 class FloatingImageService : AccessibilityService() {
 
     private lateinit var windowManager: WindowManager
-    private lateinit var floatingView: View
+    private lateinit var floatingImageLayout: View
+    private lateinit var floatingImageView: ImageView
     private lateinit var targetPositions: ArrayList<Pair<Int, Int>>
     private var currentPositionIndex = 0
     private var isShowing = false
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-        // Check if the desired app is launched
-        if (event?.packageName == "com.kakao.talk" ||  event?.packageName == "com.nhn.android.search") {
-            // Show the floating image when the desired app is launched
-            showFloatingImage(100,200)
+        if (event?.packageName == "com.kakao.talk" || event?.packageName == "com.nhn.android.search") {
+            if (!isShowing) {
+                showFloatingImage(100, 200)
+                showToast("Good!")
+                isShowing = true
+            }
         } else {
-            hideFloatingImage()
+            if (isShowing) {
+                hideFloatingImage()
+                isShowing = false
+            }
         }
     }
+
     override fun onCreate() {
         super.onCreate()
-        showFloatingImage(100, 200)
+        targetPositions = arrayListOf()
     }
 
     override fun onDestroy() {
@@ -39,14 +46,11 @@ class FloatingImageService : AccessibilityService() {
         hideFloatingImage()
     }
 
-
     override fun onInterrupt() {
         // Handle service interruption
     }
 
     private fun showFloatingImage(x: Int, y: Int) {
-        if (isShowing) return
-
         windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
         val params = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             WindowManager.LayoutParams(
@@ -69,17 +73,56 @@ class FloatingImageService : AccessibilityService() {
         params.x = x
         params.y = y
 
-        floatingView = LayoutInflater.from(this).inflate(R.layout.floating_image_layout, null)
-        windowManager.addView(floatingView, params)
-        isShowing = true
+        val layoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        floatingImageLayout = layoutInflater.inflate(R.layout.floating_image_layout, null)
+        floatingImageView = floatingImageLayout.findViewById(R.id.floating_image_view)
+
+        floatingImageView.setOnTouchListener(object : View.OnTouchListener {
+            private var initialX: Int = 0
+            private var initialY: Int = 0
+            private var initialTouchX: Float = 0f
+            private var initialTouchY: Float = 0f
+
+            override fun onTouch(v: View?, event: MotionEvent): Boolean {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        initialX = params.x
+                        initialY = params.y
+                        initialTouchX = event.rawX
+                        initialTouchY = event.rawY
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        if (currentPositionIndex < targetPositions.size) {
+                            val targetPosition = targetPositions[currentPositionIndex]
+                            params.x = targetPosition.first
+                            params.y = targetPosition.second
+                            windowManager.updateViewLayout(floatingImageLayout, params)
+
+                            currentPositionIndex++
+                        } else {
+                            onDestroy()
+                        }
+                    }
+                }
+                return false
+            }
+        })
+
+        windowManager.addView(floatingImageLayout, params)
     }
 
     private fun hideFloatingImage() {
-        if (!isShowing) return
-        windowManager.removeView(floatingView)
-        isShowing = false
+        windowManager.removeView(floatingImageLayout)
     }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
         return START_STICKY
     }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
 }
+
+
--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -6,7 +6,11 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
@@ -22,6 +26,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var currentPositionIndex = 0
 
         if (!isOverlayPermissionGranted()) {
             requestOverlayPermission()
@@ -46,8 +51,26 @@ class MainActivity : AppCompatActivity() {
             startEpisodeIntent(packageName)
             val serviceIntent = Intent(this, FloatingImageService::class.java)
             startService(serviceIntent)
-        // Add this code to properly start the service
-            //startFloatingImageService()
+            val floatingImageView = findViewById<ImageView>(R.id.floating_image_view)
+
+            floatingImageView.setOnTouchListener(object : View.OnTouchListener {
+                // 기존에 생성한 터치 리스너 코드 (변수 초기화, onTouch 메서드)
+                // ACTION_UP 경우에 대한 코드 수정
+                MotionEvent.ACTION_UP -> {
+                    if (currentPositionIndex < targetPositions.size) {
+                        val targetPosition = targetPositions[currentPositionIndex]
+                        val layoutParams =
+                            floatingImageView.layoutParams as ViewGroup.MarginLayoutParams
+                        layoutParams.setMargins(targetPosition.first, targetPosition.second, 0, 0)
+                        floatingImageView.layoutParams = layoutParams
+
+                        currentPositionIndex++
+                    } else {
+                        // ImageView를 사라지게 할 때 사용
+                        floatingImageView.visibility = View.INVISIBLE
+                    }
+                }
+            })
         }
     }
     private fun requestOverlayPermission() {
@@ -98,7 +121,25 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+    private fun setArray(arrayName: String){
+        val resourceId = resources.getIdentifier(arrayName, "array", packageName)
+        val targetPositions = ArrayList<Pair<Int, Int>>()
 
+        if (resourceId != 0) { // Check if the resource exists
+            val positionsStringArray = resources.getStringArray(resourceId)
+
+            for (positionString in positionsStringArray) {
+                val coordinates = positionString.split(",").map { it.trim().toInt() }
+                if (coordinates.size == 2) {
+                    val pair = coordinates[0] to coordinates[1]
+                    targetPositions.add(pair)
+                }
+            }
+
+        } else {
+            showToast("Error: Array not found!")
+        }
+    }
 
 
 
@@ -116,5 +157,10 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
 }
 
